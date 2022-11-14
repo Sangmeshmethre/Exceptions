e2dfd594166cc443fb61e6bd9438642c6a2059e4
Sangmesh Methre <SangmeshMetre>
2022-11-14 23:29:10 +0530

    Happy Mood

---
+++ MooodAnalyse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerAppWithCoreUC2
+{
+    public class MooodAnalyse
+    {
+        private string message;
+        ///<summary>
+        ///Parameterised Constructor
+        ///<summary>
+        ///param name="message"></param
+        public MooodAnalyse(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood()
+        {
+            try
+            {
+
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
