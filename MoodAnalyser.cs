03ca7ea5aff8f43b497fc7e190dfd0bfeb6a8fea
Sangmesh Methre <SangmeshMetre>
2022-11-14 21:41:27 +0530

    Happy Or Sad Mood

---
+++ MoodAnalyser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MoodAnalyzerAppWithCore
+{
+    public class MooodAnalyse
+    {
+        private string message;
+        ///<summary>
+        ///Parameterised Constructor
+        ///<summary>
+        ///param name="message"></param
+        public MooodAnalyse(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood()
+        {
+            if (this.message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPAY";
+            }
+        }
+    }
+}
