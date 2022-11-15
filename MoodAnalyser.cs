adcc0e8190fe573d1db32d6cd50d50075fa1cbfe
Sangmesh Methre <SangmeshMetre>
2022-11-15 23:01:48 +0530

    CustomException

---
+++ MoodAnalyser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProb
+{
+    public class MoodAnalyser
+    {
+        //Refactor to take message in Constructor
+        public string message;
+        public MoodAnalyser()
+        {
+
+        }
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood Should Not Be Empty");
+
+                }
+                if (message.ToUpper().Contains("SAD"))
+                {
+                    return "Sad";
+                }
+                else
+                {
+                    return "Happy";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NULL_MESSAGE, "Mood Should Not Be Null");
+            }
+            catch (MoodAnalyserCustomException e)
+            {
+                return "Mood Should Not Be Empty";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Program");
+
+
+            Console.WriteLine("Enter your Mood:");
+            string message = Console.ReadLine();
+            //Creating object of MoodAnalyser class
+
+            MoodAnalyser moodobj = new MoodAnalyser(message);
+            string store = moodobj.AnalyseMood();
+
+            Console.WriteLine(store + "Mood");
+        }
+    }
+}
