a8e4aad2d1d96ef1e7f120cae9999dc1d9fdee26
Sangmesh Methre <ssmcmethre3080@gmail.com>
2022-11-16 01:05:28 +0530

    Default Constucotor

---
+++ MoodAnalyser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    public class MoodAnalyser
+    {
+        //Refactor to take message in Constructor
+        public string message;
+        public MoodAnalyser()
+        {
+
+        }
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood Should Not Be Empty");
+
+                }
+                if (message.ToUpper().Contains("SAD"))
+                {
+                    return "Sad";
+                }
+                else
+                {
+                    return "Happy";
+                }
+            }
+
+            catch (MoodAnalyserCustomException)
+            {
+                return "Mood Should Not Be Empty";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Program");
+
+
+            Console.WriteLine("Enter your Mood:");
+            string message = Console.ReadLine();
+            //Creating object of MoodAnalyser class
+
+            MoodAnalyser moodobj = new MoodAnalyser(message);
+            string store = moodobj.AnalyseMood();
+
+            Console.WriteLine(store + "Mood");
+        }
+    }
+}
