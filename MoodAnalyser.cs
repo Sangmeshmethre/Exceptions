3041094942b21235fdd19a2393937763eec44818
Sangmesh Methre <ssmcmethre3080@gmail.com>
2022-11-16 01:19:58 +0530

    Reflection Parameter

---
+++ MoodAnalyser.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionUC2
+{
+    public class MoodAnalyser
+    {
+        //Refactor to take message in Constructor
+        public string message;
+        string className;
+        string constructorName;
+        public MoodAnalyser()
+        {
+
+        }
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public MoodAnalyser(string className, string constructorName)
+        {
+
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood Should Not Be Empty");
+
+                }
+                if (message.ToUpper().Contains("SAD"))
+                {
+                    return "Sad";
+                }
+                else
+                {
+                    return "Happy";
+                }
+            }
+
+            catch (MoodAnalyserCustomException obj)
+            {
+                //return "Mood Should Not Be Empty";
+                string exception = obj.Message;
+                return exception;
+            }
+        }
+    }
+}
---
+++ MoodAnalyserFactory.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReflectionUC2
+{
+    public class MoodAnalyserFactory
+    {
+        public static object CreateMoodAnalyser(string className, string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(pattern, className);
+            try
+            {
+                if (result.Success)
+                {
+                    try
+                    {
+
+                        {
+                            Assembly assembly = Assembly.GetExecutingAssembly();
+                            Type moodAnalysetype = assembly.GetType(className);
+                            return Activator.CreateInstance(moodAnalysetype);
+                        }
+                    }
+                    catch (ArgumentNullException)
+                    {
+                        Console.WriteLine("Your Input is not Valid");
+                        throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class not Found");
+                    }
+                }
+                else
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor not Found");
+                }
+            }
+            catch (MoodAnalyserCustomException e)
+            {
+                return e.Message;
+            }
+        }
+
+        //UC5
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyser);
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = ctor.Invoke(new object[] { message });
+                    return instance;
+                }
+                else
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor not Found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class not Found");
+
+            }
+        }
+
+    }
+}
