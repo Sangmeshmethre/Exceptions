2b6272d06cb5b45d1d6f380e7b19f89923d103bf
Sangmesh Methre <SangmeshMetre>
2022-11-15 23:26:40 +0530

    TestCase

---
+++ MoodAnalyserProb/MSTest_MoodAnalyser/Usings.cs
@@ -0,0 +1,12 @@
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest_MoodAnalyser
+{
+    class MoodAnalyser
+    {
+        internal string AnalyseMood(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
