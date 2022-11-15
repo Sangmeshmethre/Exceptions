34fe083ab98e97f9ce3bb7be61330a906b0d9d6e
Sangmesh Methre <ssmcmethre3080@gmail.com>
2022-11-16 00:50:08 +0530

    Refarcter

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using RepeationUC2;
+using System;
+
+namespace RepeationUC2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Program");
+
+
+            Console.WriteLine("Enter your Mood:");
+            string message = Console.ReadLine();
+            //Creating object of MoodAnalyser class
+
+            MoodAnalyser moodobj = new MoodAnalyser(message);
+
+
+            Console.WriteLine(moodobj.AnalyseMood());
+        }
+    }
+}
