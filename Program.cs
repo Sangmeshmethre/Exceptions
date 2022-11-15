39cd77751a7672f5cff26d0c2b7d0a964a2ef589
Sangmesh Methre <ssmcmethre3080@gmail.com>
2022-11-16 00:23:08 +0530

    Mood Messages In Constructr

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorMoodAnalyser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser Program");
+
+
+            Console.WriteLine("Enter your Mood:");
+            string message = Console.ReadLine();
+            //Creating object of MoodAnalyser class
+
+            MoodAnalyser moodobj = new MoodAnalyser(message);
+
+
+            Console.WriteLine(moodobj.AnalyseMood());
+        }
+    }
+}
